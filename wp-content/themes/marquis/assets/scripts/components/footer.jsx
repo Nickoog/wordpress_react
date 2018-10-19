338bb3b643863e4c62f5bce679173b80c2d4e521
Nicolas Hoog <nicolas.hoog@gmail.com>
2018-10-19 16:45:14 -0400

    footer HTML + CSS

--- wp-content/themes/marquis/assets/scripts/components/footer.jsx
+++ wp-content/themes/marquis/assets/scripts/components/footer.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
 
 const Footer = () => (
-    <footer id="colophon" className="container">
-        <div className="card-footer text-center bg-transparent border-primary">
-            Marquis Theme {(new Date().getFullYear())}
+    <footer id="colophon" className="footer">
+        <div className="container footer-nav">
+            <div className="social-container">
+                <div className="icon-wrapper">
+                    <a href="#">
+                        <i className="fa fa-facebook" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div className="icon-wrapper">
+                    <a href="#">
+                        <i className="fa fa-instagram" aria-hidden="true"></i>
+                    </a>
+                </div>
             </div>
+            <div className="footer-links-container">
+                <NavLink className="nav-link" exact activeClassName="active" to={MarquisSettings.path + "politique-de-confidentialite/"}>Plan du site</NavLink>
+                <NavLink className="nav-link" activeClassName="current" to={MarquisSettings.path + "politique-de-confidentialite/"}>Contact</NavLink>
+            </div>
+        </div>
+        <div className="copyright-wrapper">
+            <p>
+                Copyright © {(new Date().getFullYear())} Marquis Theme
+            </p>
+        </div>
     </footer>
 );
 
