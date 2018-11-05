001ab2dc83c1870501beae7c3a47ee8ddecfd68a
Nicolas Hoog <nicolas.hoog@gmail.com>
2018-11-05 13:36:25 -0500

    set 1er graphQL querie

--- wp-content/themes/marquis/assets/scripts/components/posts/posts.jsx
+++ wp-content/themes/marquis/assets/scripts/components/posts/posts.jsx
@@ -1,57 +1,20 @@
-import React from "react";
+import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
+
+//components
 import PostList from "./Post-list";
-
-const applyUpdateResult = (result) => (prevState) => ({
-    //posts: [...prevState.posts, ...result.posts],
-    page: result.page,
-});
-
-const applySetResult = (result) => ({
-    posts: result,
-    page: result.page,
-});
-
-const getPostsUrl = (page) =>
-   `${MarquisSettings.URL.api}posts/?per_page=18&page=${page}`;
 
 let totalPages;
 
-// getMorePosts = () =>{
-
-
-//     this.setState({ page: this.state.page + 1 });
-
-//     fetch(MarquisSettings.URL.api + "posts/?page=" + this.state.page)
-//     .then(function (response) {
-//         for (var pair of response.headers.entries()) {
-//         // getting the total number of pages
-//         if (pair[0] == "x-wp-totalpages") {
-//             totalPages = pair[1];
-            
-//         }
-
-//         if (that.state.page >= totalPages) {
-//             that.setState({ getPosts: false });
-//         }
-//         }
-//         if (!response.ok) {
-//             throw Error(response.statusText);
-//         }
-//         return response.json();
-//     })
-//     .then(function (results) {
-//         var allPosts = that.state.posts.slice();
-//         results.forEach(function (single) {
-//         allPosts.push(single);
-//         });
-//         that.setState({ posts: allPosts });
-//     })
-//     .catch(function (error) {
-//         console.log(
-//         "There has been a problem with your fetch operation: " + error.message
-//         );
-//     });
-// }
+const getPostsQuery = gql`
+    {
+        posts(posts:"gallery",num:5){
+            id
+            title
+        } 
+    }
+`;
 
 class Posts extends React.Component {
     constructor(props) {
@@ -61,41 +24,53 @@ class Posts extends React.Component {
             page: 1,
         };
     }
-
-    componentDidMount() {
-        window.onbeforeunload = function () {
-            window.scrollTo(0, 0);
-        };
-        this.fetchPosts(this.state.page)
-    }
-
-    fetchPosts = (page) => {
-        fetch(getPostsUrl(page))
-            .then(response => {
-                for (const pair of response.headers.entries()) {
-                    // getting the total number of pages
-                    pair[0] === "x-wp-totalpages" ? 
-                        totalPages = pair[1]: null;
-
-                }
-                return response.json();
-            })
-            .then(result => {
-                let allPosts = this.state.posts.slice();
-                result.forEach(function (single) {
-                    allPosts.push(single);
-                });
-                this.setState({
-                    posts: allPosts,
-                    page: page
-                });
-            })
-            .catch(error => {
-                console.log(
-                "There has been a problem with your fetch operation: " + error.message
+    displayPosts(){
+        let data = this.props.data;
+        console.log(data)
+        if(data.loading){
+            return( <div>Loading posts...</div> );
+        } else {
+            return data.posts.map(post => {
+                return(
+                    <li key={ post.id }>{ post.title }</li>
                 );
-            });
+            })
+        }
     }
+    // componentDidMount() {
+    //     window.onbeforeunload = function () {
+    //         window.scrollTo(0, 0);
+    //     };
+    //     this.fetchPosts(this.state.page)
+    // }
+
+    // fetchPosts = (page) => {
+    //     fetch(getPostsUrl(page))
+    //         .then(response => {
+    //             for (const pair of response.headers.entries()) {
+    //                 // getting the total number of pages
+    //                 pair[0] === "x-wp-totalpages" ? 
+    //                     totalPages = pair[1]: null;
+
+    //             }
+    //             return response.json();
+    //         })
+    //         .then(result => {
+    //             let allPosts = this.state.posts.slice();
+    //             result.forEach(function (single) {
+    //                 allPosts.push(single);
+    //             });
+    //             this.setState({
+    //                 posts: allPosts,
+    //                 page: page
+    //             });
+    //         })
+    //         .catch(error => {
+    //             console.log(
+    //             "There has been a problem with your fetch operation: " + error.message
+    //             );
+    //         });
+    // }
 
     onPaginatedSearch = (e) => 
         this.fetchPosts(this.state.page + 1);
@@ -105,15 +80,19 @@ class Posts extends React.Component {
         return (
         <div className="posts container">
             <div className="posts-container">
-                <PostList 
+                <ul>
+                    { this.displayPosts() }
+                </ul>
+                {/* <PostList 
                     posts={this.state.posts}
                     page={this.state.page}
                     totalPages={totalPages}
-                    onPaginatedSearch={this.onPaginatedSearch} />
+                    onPaginatedSearch={this.onPaginatedSearch} /> */}
             </div>
         </div>
         );
     }
 }
 
-export default Posts;
+export default graphql(getPostsQuery)(Posts);
+
--- wp-content/themes/marquis/assets/scripts/index.jsx
+++ wp-content/themes/marquis/assets/scripts/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
+// components
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/home/Home';
@@ -11,24 +14,32 @@ import Galleries from './components/gallery/Galleries';
 import Gallery from './components/gallery/Gallery';
 import Page from './components/Page';
 
+// package CSS
 import "animate.css/animate.min.css";
 
+// apollo client setup
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql'
+});
+
 const App = () => (
-    <div id="page-inner">
-        <Header />
-            <div id="content">
-                <Switch>
-                    <Route exact path={MarquisSettings.path} component={Home} />
-                    <Route exact path={MarquisSettings.path + 'posts'} component={Posts} />
-                    <Route exact path={MarquisSettings.path + 'posts/:slug'} component={Post} />
-                    <Route exact path={MarquisSettings.path + 'gallery'} component={Galleries} />
-                    <Route exact path={MarquisSettings.path + 'gallery/:slug'} component={Gallery} />
-                    <Route exact path={MarquisSettings.path + ':slug'} component={Page} />
-                    {/* <Route path="*" component={NotFound} /> */}
-                </Switch>   
-            </div>
-        <Footer />
-    </div>
+    <ApolloProvider client={client}>
+        <div id="page-inner">
+            <Header />
+                <div id="content">
+                    <Switch>
+                        <Route exact path={MarquisSettings.path} component={Home} />
+                        <Route exact path={MarquisSettings.path + 'posts'} component={Posts} />
+                        <Route exact path={MarquisSettings.path + 'posts/:slug'} component={Post} />
+                        <Route exact path={MarquisSettings.path + 'gallery'} component={Galleries} />
+                        <Route exact path={MarquisSettings.path + 'gallery/:slug'} component={Gallery} />
+                        <Route exact path={MarquisSettings.path + ':slug'} component={Page} />
+                        {/* <Route path="*" component={NotFound} /> */}
+                    </Switch>   
+                </div>
+            <Footer />
+        </div>
+    </ApolloProvider>
 );
 
 // Routes
